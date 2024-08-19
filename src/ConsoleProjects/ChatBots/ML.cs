c39d0bb54a22f8166bf9b22f3e7ca3567a3f80b1
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-08-19 19:14:58 +0500

    console project

---
+++ src/ConsoleProjects/ChatBots/ML.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ML.OnnxRuntimeGenAI;
+using System;
+
+namespace ChatBots;
+
+internal static class ML
+{
+    public static void MLTest()
+    {
+        //string modelPath = @"C:\Users\khurram\.aitk\models\microsoft\Phi-3-mini-128k-instruct-onnx\directml\directml-int4-awq-block-128";
+        //string modelPath = @"C:\Users\khurram\.aitk\models\microsoft\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32-acc-level-4";
+        string modelPath = @"Models\directml\directml-int4-awq-block-128";
+        Console.Write("Loading model from " + modelPath + "...");
+        using Model model = new(modelPath);
+        Console.Write("Done\n");
+
+        using Tokenizer tokenizer = new(model);
+        using TokenizerStream tokenizerStream = tokenizer.CreateStream();
+
+        while (true)
+        {
+            Console.Write("User: ");
+            string? input = Console.ReadLine();
+
+            if (input == "quit") break;
+
+            string prompt = "<|user|>\n" + input + "<|end|>\n<|assistant|>";
+
+            var sequences = tokenizer.Encode(prompt);
+
+            using GeneratorParams generatorParams = new GeneratorParams(model);
+            generatorParams.SetSearchOption("max_length", 200);
+            generatorParams.SetInputSequences(sequences);
+
+            Console.Out.Write("\nAI: ");
+            using Generator generator = new(model, generatorParams);
+            while (!generator.IsDone())
+            {
+                generator.ComputeLogits();
+                generator.GenerateNextToken();
+                Console.Out.Write(tokenizerStream.Decode(generator.GetSequence(0)[^1]));
+                Console.Out.Flush();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/ConsoleProjects/ChatBots/OpenAI.cs
@@ -0,0 +1,36 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System;
+using System.ClientModel;
+using System.Linq;
+
+namespace ChatBots;
+
+internal static class OpenAI
+{
+    public static void Run()
+    {
+        var options = new OpenAIClientOptions
+        {
+            Endpoint = new Uri("http://127.0.0.1:11434/v1") // Ollama
+            // Endpoint = new Uri("http://127.0.0.1:5272/v1") // AI Toolkit
+        };
+
+        var openAIClient = new OpenAIClient(new ApiKeyCredential("x"), options);
+        var chatClient = openAIClient.GetChatClient("phi3:mini");
+        //var chatClient = openAIClient.GetChatClient("Phi-3-mini-128k-cpu-int4-rtn-block-32-onnx");
+
+        var chatCompletion = chatClient.CompleteChat(
+            [
+                new SystemChatMessage("You are a helpful assistant. Be brief and succinct."),
+                new UserChatMessage("What is the golden ratio?")
+            ], new ChatCompletionOptions
+            {
+                Temperature = 0.7f 
+            }
+            ).Value;
+
+        foreach (var content in chatCompletion.Content.Where(o => null != o && !string.IsNullOrEmpty(o.Text)))
+            Console.WriteLine(content.Text);
+    }
+}
