5b5bfb6db0739819195cd423f1c5f7a2023d3a83
khurram-uworx <khurram.aziz@uworx.co.uk>
2024-11-20 14:33:39 +0500

    rpa

---
+++ src/Rpa/Calculators.cs
@@ -0,0 +1,93 @@
+﻿using FlaUI.Core.AutomationElements;
+using System.Text.RegularExpressions;
+
+namespace Rpa;
+
+public interface ICalculator
+{
+    Button Button1 { get; }
+    Button Button2 { get; }
+    Button Button3 { get; }
+    Button Button4 { get; }
+    Button Button5 { get; }
+    Button Button6 { get; }
+    Button Button7 { get; }
+    Button Button8 { get; }
+    Button ButtonAdd { get; }
+    Button ButtonEquals { get; }
+    string Result { get; }
+}
+
+public class LegacyCalc : ICalculator
+{
+    private readonly AutomationElement _mainWindow;
+
+    public Button Button1 => FindElement("1").AsButton();
+    public Button Button2 => FindElement("2").AsButton();
+    public Button Button3 => FindElement("3").AsButton();
+    public Button Button4 => FindElement("4").AsButton();
+    public Button Button5 => FindElement("5").AsButton();
+    public Button Button6 => FindElement("6").AsButton();
+    public Button Button7 => FindElement("7").AsButton();
+    public Button Button8 => FindElement("8").AsButton();
+    public Button ButtonAdd => FindElement("Add").AsButton();
+    public Button ButtonEquals => FindElement("Equals").AsButton();
+
+    public string Result
+    {
+        get
+        {
+            var resultElement = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("158"));
+            var value = resultElement.Properties.Name;
+            return Regex.Replace(value, "[^0-9]", String.Empty);
+        }
+    }
+
+    public LegacyCalc(AutomationElement mainWindow)
+    {
+        _mainWindow = mainWindow;
+    }
+
+    private AutomationElement FindElement(string text)
+    {
+        var element = _mainWindow.FindFirstDescendant(cf => cf.ByText(text));
+        return element;
+    }
+}
+
+public class Win10Calc : ICalculator
+{
+    private readonly AutomationElement _mainWindow;
+
+    public Button Button1 => FindElement("num1Button").AsButton();
+    public Button Button2 => FindElement("num2Button").AsButton();
+    public Button Button3 => FindElement("num3Button").AsButton();
+    public Button Button4 => FindElement("num4Button").AsButton();
+    public Button Button5 => FindElement("num5Button").AsButton();
+    public Button Button6 => FindElement("num6Button").AsButton();
+    public Button Button7 => FindElement("num7Button").AsButton();
+    public Button Button8 => FindElement("num8Button").AsButton();
+    public Button ButtonAdd => FindElement("plusButton").AsButton();
+    public Button ButtonEquals => FindElement("equalButton").AsButton();
+
+    public string Result
+    {
+        get
+        {
+            var resultElement = FindElement("CalculatorResults");
+            var value = resultElement.Properties.Name;
+            return Regex.Replace(value, "[^0-9]", String.Empty);
+        }
+    }
+
+    public Win10Calc(AutomationElement mainWindow)
+    {
+        _mainWindow = mainWindow;
+    }
+
+    private AutomationElement FindElement(string text)
+    {
+        var element = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId(text));
+        return element;
+    }
+}
---
+++ src/Rpa/Program.cs
@@ -0,0 +1,60 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.Input;
+using FlaUI.Core.WindowsAPI;
+using FlaUI.UIA3;
+using Rpa;
+using OperatingSystem = FlaUI.Core.Tools.OperatingSystem;
+
+//Application application = null;
+//if (OperatingSystem.IsWindows10())
+//    // Use the store application on those systems
+//    application = Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+//else if (OperatingSystem.IsWindowsServer2016() || OperatingSystem.IsWindowsServer2019())
+//    // The calc.exe on this system is just a stub which launches win32calc.exe
+//    application = Application.Launch("win32calc.exe");
+//else
+//    application = Application.Launch("calc.exe");
+
+//Application application = OperatingSystem.IsWindows10()
+//    ? Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App")
+//    : OperatingSystem.IsWindowsServer2016() || OperatingSystem.IsWindowsServer2019()
+//        ? Application.Launch("win32calc.exe")
+//        : Application.Launch("calc.exe");
+
+Application application = OperatingSystem.IsWindows10() switch
+{
+    true => Application.LaunchStoreApp("Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"),
+    false when OperatingSystem.IsWindowsServer2016() || OperatingSystem.IsWindowsServer2019() => Application.Launch("win32calc.exe"),
+    _ => Application.Launch("calc.exe")
+};
+
+var window = application.GetMainWindow(new UIA3Automation());
+var calc = (OperatingSystem.IsWindows10() || OperatingSystem.IsWindows11()) ? (ICalculator)new Win10Calc(window) : new LegacyCalc(window);
+
+// Switch to default mode
+Thread.Sleep(1000);
+Keyboard.TypeSimultaneously(VirtualKeyShort.ALT, VirtualKeyShort.KEY_1);
+Wait.UntilInputIsProcessed();
+application.WaitWhileBusy();
+Thread.Sleep(1000);
+
+// Simple addition
+calc.Button1.Click();
+calc.Button2.Click();
+calc.Button3.Click();
+calc.Button4.Click();
+calc.ButtonAdd.Click();
+calc.Button5.Click();
+calc.Button6.Click();
+calc.Button7.Click();
+calc.Button8.Click();
+calc.ButtonEquals.Click();
+application.WaitWhileBusy();
+var result = calc.Result;
+
+//https://github.com/FlaUI/FlaUI/blob/master/src/FlaUI.Core.UITests/CalculatorTests.cs
+//Assert.That(result, Is.EqualTo("6912"));
+
+Console.WriteLine($"Result from Windows Calculator: {result}, press enter to close it!");
+Console.ReadLine();
+application.Close();
